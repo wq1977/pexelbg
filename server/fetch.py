5360b876c150c4e8b831b03080fa4b2004705fbf
wesley <wangqiang@ofo.so>
2017-08-14 15:18:52 +0800

    change something

--- server/fetch.py
+++ server/fetch.py
@@ -5,22 +5,8 @@ import urllib.request
 import datetime
 import re
 
-seed = datetime.datetime.utcnow().strftime('%Y-%m-%d+%H%%3A%M%%3A%S++0000')
-url = 'https://www.pexels.com/?format=js&page=1&seed='+seed
-req = urllib.request.Request(
-    url, 
-    data=None, 
-    headers={
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-    }
-)
-
-f = urllib.request.urlopen(req)
-cnt = f.read().decode('utf-8')
-
-all = re.findall("<img width=\\\\\"([0-9]*)\\\\\" height=\\\\\"([0-9]*)\\\\\" style=\\\\\"[^\"]*\\\\\" alt=\\\\\"([^\"]*)\\\\\" data-pin-media=\\\\\"[^\"]*\\\\\" src=\\\\\"(https:\/\/images.pexels.com\/photos\/[0-9]*\/[a-zA-Z0-9-]*.jpg)", cnt)
-
 def save(photo):
+    """保存图片"""
     if photo[1] == '0':
         return
     tags = photo[2]
@@ -40,5 +26,21 @@ def save(photo):
     resp = urllib.request.urlopen(req)
     resp.read()
 
-for photo in all:
-    save(photo)
+seed = datetime.datetime.utcnow().strftime('%Y-%m-%d+%H%%3A%M%%3A%S++0000')
+for page in range(1,7):
+    url = 'https://www.pexels.com/?format=js&page=%d&seed=%s' % (page, seed)
+    req = urllib.request.Request(
+        url, 
+        data=None, 
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        }
+    )
+
+    f = urllib.request.urlopen(req)
+    cnt = f.read().decode('utf-8')
+
+    all = re.findall("<img width=\\\\\"([0-9]*)\\\\\" height=\\\\\"([0-9]*)\\\\\" style=\\\\\"[^\"]*\\\\\" alt=\\\\\"([^\"]*)\\\\\" data-pin-media=\\\\\"[^\"]*\\\\\" src=\\\\\"(https:\/\/images.pexels.com\/photos\/[0-9]*\/[a-zA-Z0-9-]*.jpg)", cnt)
+
+    for photo in all:
+        save(photo)
